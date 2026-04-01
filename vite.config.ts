7c1e648d6aaef6b5e0fc24016cfb64fd940d8741
Jason LU <luqiao2015@gmail.com>
2026-04-01 23:47:31 +0200

    add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/clawshow-lyon-appartement-1775080039/',
+})
